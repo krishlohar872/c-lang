caff7368e7e96a930a1668f7e1580335e8d7d98d
krishlohar872 <krishlohar872@gmail.com>
2024-09-06 09:20:18 -0400

    Add files via upload

---
+++ krish/first and last digit.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+	int num, first , last, original;
+	printf("enter a number :");
+	scanf ("%d ", &num);
+	
+	original = num ;
+	
+	last = num % 10;
+	
+	while (num >= 10)
+	
+	{
+		num = num / 10;
+	
+	 } 
+	 first = num ;
+	 
+	 int sum = first + last;
+	 printf(" sum %d is %d/n",original , sum);
+	 
+}
